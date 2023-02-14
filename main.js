72ea983e885917bada526e10ddaf12170167545e
Bladeo8x <gerardo50_@hotmail.com>
2023-02-14 21:58:59 +0700

    Adding var, const, modify the display of the generator for easy understanding, including, uppercase, lowercase, numbers, and special characters.

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-// alert();
+const includeUppercaseElement = document.getElementById("Include-Uppercase");
+const includeNumbersElement = document.getElementById("Include-Numbers");
+const includeSymbolsElement = document.getElementById(
+  "Include-SpecialCharacters"
+);
+const form = document.getElementById("Password-GeneratorForm");
+const PasswordDisplay = document.getElementById("PasswordDisplay");
+
+// Assignment of variables
+
+const LowerCase = arrayFromLowToHigh(97, 122);
+const UpperCase = arrayFromLowToHigh(65, 90);
+const Numbers = arrayFromLowToHigh(48, 57);
+const SpecialCharacters = arrayFromLowToHigh(33, 47)
+  .concat(arrayFromLowToHigh(58, 64))
+  .concat(arrayFromLowToHigh(91, 96))
+  .concat(arrayFromLowToHigh(123, 126));
